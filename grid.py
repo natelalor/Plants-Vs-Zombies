9aa02cc0750666150892e50d2d4ba10cb3b3b13d
Nate Lalor <natelalor@gmail.com>
2023-02-27 21:36:15 -0500

    Created grid class and square class. provided simple constructors.

---
+++ grid.py
@@ -0,0 +1,14 @@
+
+
+class Grid:
+    '''
+    Facilitates organizational structure of playing field. A list of lists of square objects.
+        __init__: defines size of grid with x & y (is not a square)
+    '''
+    def __init__(self, size_columns, size_rows):
+        self.size_columns = size_columns
+        self.size_rows = size_rows
+
+
+
+
