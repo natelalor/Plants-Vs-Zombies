231c44bb7ce58c2a560ef5c71e72f9389cf67e2e
Rmart7866 <rmarti20@uvm.edu>
2023-04-23 20:57:08 -0400

    defender choice GUI

--- choose_defenders.py
+++ choose_defenders.py
@@ -1,5 +1,4 @@
 import time
-
 import arcade
 import constants as c
 from game import Game
@@ -13,15 +12,19 @@ class ChooseDefenders(arcade.View):
         self.manager.enable()
         self.chosen_defenders = []
         self.defenders_list = []
+        self.gui_buttons = []
         self.v_box = arcade.gui.UIBoxLayout()
+        self.background = arcade.load_texture("images/garden.jpg")
+        self.num_defenders = 5
 
         for defender in c.defenders_data.keys():
             self.defenders_list.append(defender)
 
+
         while self.defenders_list:
             i = 0
             tmp = arcade.gui.UIBoxLayout(vertical=False)
-            for column in range(4):
+            for column in range(9):
                 if len(self.defenders_list) != 0:
                     defender = c.defenders_data[self.defenders_list[0]]
                     button = arcade.gui.UITextureButton(texture=arcade.load_texture("GUI/"+defender['name']+".png"),
@@ -30,12 +33,19 @@ class ChooseDefenders(arcade.View):
                                                         texture_pressed=arcade.load_texture(
                                                             "GUI/"+defender['name']+"-selected.png"),
                                                         width=75, height=75)
-                    button.defenderId = self.defenders_list[0]
 
+                    button.defenderId = self.defenders_list[0]
+                    button.cost = defender['cost']
+                    # used to determine if its selcted
+                    button.selected = False
+                    self.gui_buttons.append(button)
+                    # holds all teh GUI buttons in a box
                     tmp.add(
                         arcade.gui.UIBorder(button.with_border(color=(117, 35, 19, 255)), border_width=10,
                                             border_color=(117, 35, 19, 255)))
+
                     button.on_click = self.on_choose
+
                     self.defenders_list.pop(0)
 
                 else:
@@ -44,31 +54,63 @@ class ChooseDefenders(arcade.View):
 
         self.manager.add(
             arcade.gui.UIAnchorWidget(
-                anchor_x="center_x",
-                anchor_y="center_y",
+                anchor_x="left",
+                anchor_y="top",
                 child=self.v_box)
         )
 
         texture_hovered = arcade.load_texture("GUI/StartButton_hover.png")
         texture_selected = arcade.load_texture("GUI/StartButton_selected.png")
         texture_unselected = arcade.load_texture("GUI/StartButton.png")
-        self.start_button = arcade.gui.UITextureButton(texture=texture_unselected, texture_hovered=texture_hovered, texture_selected=texture_selected)
+        self.start_button = arcade.gui.UITextureButton(texture=texture_unselected, texture_hovered=texture_hovered, texture_pressed=texture_selected )
         self.start_button.on_click = self.start_game
+
 
 
 
     def on_draw(self):
         self.clear()
+        arcade.draw_lrwh_rectangle_textured(0, 0, c.SCREEN_WIDTH, c.SCREEN_HEIGHT, self.background)
+        color = (64, 24, 17)
+        width = 165 * len(self.gui_buttons)
+        # height only needs to be changed if we have more than 9 defenders
+        arcade.draw_rectangle_filled(100, 585, width, 100, color)
+
         self.manager.draw()
 
-    def on_choose(self, event):
 
-        # loading_screen = LoadingScreen(self.window)
-        # self.window.show_view(loading_screen)
-        if event.source.defenderId not in self.chosen_defenders:
+        # GUI SLOTS
+        for i, button in enumerate(self.gui_buttons):
+
+            if self.gui_buttons[i].selected:  # selection graphic
+                arcade.draw_rectangle_outline(center_x=50 + i * 100 - i, center_y=581, color=(77, 123, 48, 255),
+                                                width=76,
+                                                height=76, border_width=5)
+            arcade.draw_text(button.cost, 23.5 + i * 100, 535, arcade.color.WHITE, 20, 40,
+                             'center')  # draw the price
+        # text and text background
+        arcade.draw_rectangle_filled(center_x=485, center_y = 300, color=(0,0,0,100), width=510, height=60)
+        arcade.draw_text('Select ' + str(self.num_defenders) + ' more defenders to start the game!', 230,300.0,
+                         arcade.color.WHITE,20,50,'left', font_name="Kenny Mini Square")
+
+
+
+
+    def on_choose(self, event):
+        # only 5 plants can be selected at a time
+        if event.source.defenderId not in self.chosen_defenders and len(self.chosen_defenders) < c.NUMBER_OF_DEFENDERS:
             self.chosen_defenders.append(event.source.defenderId)
-        else:
+            event.source.selected = True
+            self.num_defenders = 5 - len(self.chosen_defenders)
+        elif event.source.defenderId in self.chosen_defenders:
             self.chosen_defenders.remove(event.source.defenderId)
+            event.source.selected = False
+            self.num_defenders = 5 - len(self.chosen_defenders)
+
+
+
+
+
         for i in self.chosen_defenders:
             print("CHOOSE DEFENDERS:", i, end=', ')
         print()
@@ -81,6 +123,7 @@ class ChooseDefenders(arcade.View):
                     anchor_y="bottom",
                     child=self.start_button)
             )
+
 
     def start_game(self, event):
         if len(self.chosen_defenders) == c.NUMBER_OF_DEFENDERS:
