f222e4bf062a8f6bbfad11d8c272db7f745b3364
Nate Lalor <natelalor@gmail.com>
2023-06-09 18:50:00 -0400

    Initial Transfer Request

--- main.py
+++ main.py
@@ -8,18 +8,10 @@ from start_screen import StartScreen
 
 
 def main():
-    # commented this out for now as it was making extra windows
-    # tests.testLevelCreation()
-    # arcade.set_background_color(arcade.color.OLIVE)
-    # arcade.start_render()
-    # arcade.finish_render()
-    # game = Game(1)
     window = arcade.Window(c.SCREEN_WIDTH, c.SCREEN_HEIGHT, c.SCREEN_TITLE)
     start_view = StartScreen(window)
     window.show_view(start_view)
-    # game.setup()
     arcade.run()
-    # entrance()
 
 
 if __name__ == '__main__':
